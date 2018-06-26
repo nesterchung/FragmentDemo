666ba0962d3c03d6f4974e601abe802d5889606a
nester <twntwn@gmail.com>
2018-06-26 09:20:06 +0800

    Initial commit

---
+++ app/src/main/java/com/nexterchung/fragmentdemo/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.nexterchung.fragmentdemo
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.TextView
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+class MainActivity : AppCompatActivity(), AnkoLogger {
+    private val fragments: MutableMap<String, Class<out Fragment>> = mutableMapOf()
+
+    init {
+        fragments["one"] = FragmentOne::class.java
+        fragments["two"] = FragmentTwo::class.java
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        info { "onCreate" }
+        setContentView(R.layout.activity_main)
+
+        switchToFragment("one")
+
+        findViewById<RadioGroup>(R.id.radioGroup).setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radio_one -> switchToFragment("one")
+                R.id.radio_two -> switchToFragment("two")
+                else -> Unit
+            }
+        }
+    }
+
+    private fun switchToFragment(tag: String) {
+        info { "switchToFragment $tag" }
+        supportFragmentManager.findFragmentByTag(tag).let {
+            if (it == null) {
+                supportFragmentManager.beginTransaction().add(R.id.content_fragment, fragments[tag]?.newInstance(), tag).commit()
+            } else {
+                supportFragmentManager.beginTransaction().show(it).commit()
+            }
+        }
+    }
+}
+
+
+open class BaseFragment : Fragment(), AnkoLogger {
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        info { "onAttach" }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        info { "onActivityCreated" }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        info { "onCreateView" }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
+
+class FragmentOne : BaseFragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        info { "onCreateView" }
+        return TextView(context).apply {
+            text = "FragmentOne"
+        }
+    }
+}
+
+class FragmentTwo : BaseFragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        info { "onCreateView" }
+        return TextView(context).apply {
+            text = "FragmentTwo"
+        }
+    }
+}
